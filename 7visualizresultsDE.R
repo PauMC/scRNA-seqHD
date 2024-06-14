c5640a77dbf53416962d417f28a5af4c269d9243
PauMC <147192262+PauMC@users.noreply.github.com>
2024-06-14 12:52:01 +0200

    Update 7visualizresultsDE.R

--- 7visualizresultsDE.R
+++ 7visualizresultsDE.R
@@ -1,3 +1,4 @@
+
 # Librerias
 library(readr)
 library(dplyr)
@@ -53,7 +54,7 @@ ggplot(resultados_df, aes(x = Valor, y = Tipocelular, fill = Tipo)) +
   theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust = 1))
 
 
-# EnhancedVolcano y Gene ontology (GO) para los tipos celulares con mas cambios
+# EnhancedVolcano para los tipos celulares con mas cambios
 # Función para crear gráficos EnhancedVolcano
 createEnhancedVolcano <- function(data, title) {
   EnhancedVolcano(data, 
@@ -77,44 +78,35 @@ createEnhancedVolcano <- function(data, title) {
                   legendIconSize = 4.0)
 }
 
-#Gene ontology (GO) 
-# Biomart
-biomart <- read.csv("mart_export38.txt")
-
-# Función para realizar el análisis de enriquecimiento de GO y generar los gráficos
-perform_GO_analysis <- function(data, cell_type) {
-  # Filtrar los genes DE
-  data_filtrado_down <- filter(data, p_val_adj <= 0.05 & avg_log2FC < 0)
-  data_filtrado_up <- filter(data, p_val_adj <= 0.05 & avg_log2FC > 0)
-  
-  # Filtrar el biomart para los genes de interés
-  biomart_filtrado_down <- biomart[biomart$Gene.name %in% data_filtrado_down$gene, ]
-  biomart_filtrado_up <- biomart[biomart$Gene.name %in% data_filtrado_up$gene, ]
-  
-  # Realizar el análisis de enriquecimiento de GO
-  GO_upregulated <- enrichGO(gene = unique(biomart_filtrado_down$Gene.stable.ID), OrgDb = "org.Hs.eg.db", keyType = "ENSEMBL", ont = "ALL")
-  GO_downregulated <- enrichGO(gene = unique(biomart_filtrado_up$Gene.stable.ID), OrgDb = "org.Hs.eg.db", keyType = "ENSEMBL", ont = "ALL")
-  
-  # Configurar colores
-  options(enrichplot.colours = c("orange", "green"))
-  
-  # Crear los gráficos
-  a <- dotplot(GO_upregulated, showCategory = 10, font.size = 9, label_format = 45) +
-    facet_wrap("ONTOLOGY") +
-    ggtitle(paste("GO_results", cell_type, "upregulated"))
-  
-  b <- dotplot(GO_downregulated, showCategory = 10, font.size = 9, label_format = 45) +
-    facet_wrap("ONTOLOGY") +
-    ggtitle(paste("GO_results", cell_type, "downregulated"))
-  
-  return(grid.arrange(a, b, ncol = 2))
-}
-  
 linfTyNK_CD8<-read_csv("~/sc-RNAseq1/analisis/3DE/de_markers_linfTyNK_CD8_MAST.csv")
 linfTyNK_CD4<-read_csv("~/sc-RNAseq1/analisis/3DE/de_markers_linfTyNK_CD4_MAST.csv")
 
 createEnhancedVolcano(linfTyNK_CD8, "linfTyNK_CD8")
 createEnhancedVolcano(linfTyNK_CD4, "linfTyNK_CD4")
 
-perform_GO_analysis(linfTyNK_CD8, "linfTyNK_CD8")
-perform_GO_analysis(linfTyNK_CD4, "linfTyNK_CD4")
+# Caso monocitos CD14 gen IFI6
+mono_CD14<-read_csv("~/sc-RNAseq1/analisis/3DE/de_markers_monoDC_CD14_MAST.csv")
+createEnhancedVolcanoselect <- function(data, title) {
+  EnhancedVolcano(data, 
+                  lab = data$gene,
+                  x = 'avg_log2FC',
+                  y = 'p_val_adj',
+                  title = title,
+                  xlim = c(min(data$avg_log2FC, na.rm = TRUE) - 0.5, max(data$avg_log2FC, na.rm = TRUE) + 0.5),
+                  ylim = c(0, max(-log10(data$p_val_adj), na.rm = TRUE) + 0.5),
+                  xlab = bquote(~Log[2] ~ "fold change"),
+                  pCutoff = 0.05,
+                  FCcutoff = 0,
+                  cutoffLineType = 'twodash',
+                  cutoffLineWidth = 0.8,
+                  pointSize = 2.0,
+                  labSize = 3,
+                  colAlpha = 1,
+                  legendLabels = c('Not sig.','Log (base 2) FC','p-adj', 'p-adj & Log (base 2) FC'),
+                  legendPosition = 'right',
+                  legendLabSize = 10,
+                  legendIconSize = 4.0,
+                  selectLab = c('IFI6'))
+}
+
+createEnhancedVolcanoselect(mono_CD14, "mono_CD14")
